eb28ebebd9115225bcf7d7e81a18e5d4cc3fc1d5
Miryam Chaabouni <chaabouni@crxmarkets.com>
2020-07-08 09:47:26 +0200

    Chapter 2.3 Validating form input

--- src/main/java/sla/tacocloud/tacos/Taco.java
+++ src/main/java/sla/tacocloud/tacos/Taco.java
@@ -1,6 +1,8 @@
 package sla.tacocloud.tacos;
 
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.ToString;
 
@@ -8,7 +10,11 @@ import lombok.ToString;
 @ToString
 public class Taco {
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
 
 }
